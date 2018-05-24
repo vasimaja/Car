4d1705615f0cc2f8b700f0eb7a736224d3319c80
Maja <majavasiljevic88@hotmail.com>
2018-05-24 09:23:32 +0200

    izmenjeno

--- src/main.java
+++ src/main.java
@@ -6,13 +6,19 @@ public class main {
         
         Car myCar = new Car();
         System.out.println("Maksimalna brzina je: " + myCar.maxSpeed);
-        
+        System.out.println(myCar.condition);
+        System.out.println(myCar.currentPrice);
+        System.out.println(myCar.license);
+        System.out.println(myCar.minSpeed);
+        System.out.println(myCar.weight);
+
         Car companyCar = new Car(150,   30, 2000,   false,  "EFG-456",   'B',   3500,   60000);
         System.out.println("Maksimalna brzina je: " + companyCar.maxSpeed);
-        
-        
-        
-        
+        System.out.println(companyCar.condition);
+        System.out.println(companyCar.currentPrice);
+        System.out.println(companyCar.license);
+        System.out.println(companyCar.minSpeed);
+        System.out.println(companyCar.weight);
     }
     
 }
